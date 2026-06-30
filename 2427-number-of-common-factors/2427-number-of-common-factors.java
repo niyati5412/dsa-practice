a71e766b2503f14f2dce0657bf7915cfaa5ac37f
NIYATI GUPTA <niyatigupta5412@gmail.com>
2026-06-30 16:42:17 +0530

    Time: 0 ms (100%), Space: 41.9 MB (88.28%) - LeetHub

--- 2427-number-of-common-factors/2427-number-of-common-factors.java
+++ 2427-number-of-common-factors/2427-number-of-common-factors.java
@@ -1,11 +1,21 @@
 class Solution {
+
+    public int gcd(int a,int b){
+      while(b!=0){
+        int temp=b;
+        b=a%b;
+        a=temp;
+      }
+      return a;
+    }
     public int commonFactors(int a, int b) {
-       int count=0;
-       for(int i=1;i<=Math.min(a,b);i++){
-        if(a%i==0 && b%i==0){
-            count++;
+        int g = gcd(a,b);
+        int count=0;
+        for(int i=1;i<=g;i++){
+            if(g%i==0){
+                count++;
+            }
         }
-       }
-       return count;
+        return count;
     }
 }
