a6681898665d05ad9f88b85fbd092a0914f3f833
NIYATI GUPTA <niyatigupta5412@gmail.com>
2026-07-12 01:44:17 +0530

    Time: 2 ms (63.5%), Space: 42.9 MB (69.73%) - LeetHub

---
+++ 1832-check-if-the-sentence-is-pangram/1832-check-if-the-sentence-is-pangram.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean checkIfPangram(String sentence) {
+
+        boolean[] seen = new boolean[26];
+        int count = 0;
+
+        for (int i = 0; i < sentence.length(); i++) {
+            int index = sentence.charAt(i) - 'a';
+
+            if (!seen[index]) {
+                seen[index] = true;
+                count++;
+            }
+        }
+
+        return count == 26;
+    }
+}
