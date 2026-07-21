1487068f2f458f7e0b5719d5d0b6ec66ef9e6439
NIYATI GUPTA <niyatigupta5412@gmail.com>
2026-07-21 23:39:13 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Find the Frequency/find-the-frequency.java
+++ Difficulty: Easy/Find the Frequency/find-the-frequency.java
@@ -3,10 +3,10 @@ class Solution {
         // code here
         int count=0;
         for(int i=0;i<arr.length;i++){
-            if(arr[i]==x){
-                count++;
-            }
+            if(arr[i]==x)
+            count++;
         }
         return count;
+        
     }
 }
