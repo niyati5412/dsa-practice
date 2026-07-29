3612999163302599aaeb86c4eb30a080fef4359a
NIYATI GUPTA <niyatigupta5412@gmail.com>
2026-07-30 01:41:57 +0530

    Time: 1 ms (30.69%), Space: 43.5 MB (6.37%) - LeetHub

---
+++ 0058-length-of-last-word/0058-length-of-last-word.java
@@ -0,0 +1,8 @@
+class Solution {
+    public int lengthOfLastWord(String s) {
+        String words[]= s.trim().split(" ");
+        String last= words[words.length-1];
+        return last.length();
+
+    }
+}
