0c55297ff55ee47b99fd547dd08d203ee8f83366
NIYATI GUPTA <niyatigupta5412@gmail.com>
2026-07-30 17:38:39 +0530

    Time: 2 ms (100%), Space: 48.3 MB (45.74%) - LeetHub

---
+++ 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.java
@@ -0,0 +1,25 @@
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+
+        int rows = matrix.length;
+        int cols = matrix[0].length;
+
+        int row = 0;
+        int col = cols - 1;   // Start from top-right
+
+        while (row < rows && col >= 0) {
+
+            if (matrix[row][col] == target) {
+                return true;
+            }
+            else if (matrix[row][col] > target) {
+                col--;          // Move left
+            }
+            else {
+                row++;          // Move down
+            }
+        }
+
+        return false;
+    }
+}
